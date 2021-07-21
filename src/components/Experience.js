baf3ff073bba4ee706344b464e07af43703a6995
David Rawson <dm.rawson@gmail.com>
2021-07-21 22:43:10 +0100

    Map embedded in contact form

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -5,7 +5,7 @@ import { experiences } from "../data";
 const Experience = () => {
   return (
     <section id="experience">
-      <div className="container px-5 py-10 mx--auto text-center">
+      <div className="container px-5 py-10 mx-auto text-center">
         <UsersIcon className="w-10 inline-block mb-4" />
         <h1 className="sm:text-4xl text-3xl font-medium title-font text=white mb-12">
           Experience
